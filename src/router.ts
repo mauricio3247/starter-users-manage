91a076020f8d4bbb91b3c31b7dd29437961cc600
Mauricio Oropeza <mauricio.3247@gmail.com>
2019-11-28 21:10:12 -0400

    add env-exanple, add user routes to main router

--- src/router.ts
+++ src/router.ts
@@ -1,5 +1,7 @@
 import express from 'express';
 import authRouter from '@modules/auth/auth.routes'
+import userRouter from '@modules/user/user.routes'
 export default (app: express.Application) => {
   app.use('/auth', authRouter)
+  app.use('/users', userRouter)
 }
